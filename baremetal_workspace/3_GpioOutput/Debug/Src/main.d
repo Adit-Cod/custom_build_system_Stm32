6e5fb71a240fb93a0229c59740a50c9b2f58be74
Aditya <aditya.subramanian97@gmail.com>
2023-01-05 10:48:21 -0500

    Integrating CMSIS and testing functionality with blinker.

---
+++ baremetal_workspace/3_GpioOutput/Debug/Src/main.d
@@ -0,0 +1,22 @@
+Src/main.o: ../Src/main.c \
+ C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+
+C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/Ad_Docs/Embedded\ Systems/custom_build_system_Stm32/custom_build_system_Stm32/baremetal_workspace/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
